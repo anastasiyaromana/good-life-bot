473f1de30f7aeb288fe6fe4dc7035e8af9c80ab0
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2025-12-26 18:19:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ.get["BOT_TOKEN", "8571782718:AAHIR4xEKr0NrPQU7eQIfZBpHQJ4PKsI_OU"]
 
 DEFAULT_TZ = os.environ.get("DEFAULT_TZ", "Europe/Moscow")
 INACTIVE_DAYS = int(os.environ.get("INACTIVE_DAYS", "7"))
 NUDGE_COOLDOWN_DAYS = int(os.environ.get("NUDGE_COOLDOWN_DAYS", "7"))
+
