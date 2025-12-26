1ba4b49727fd0b37d7deabebcf3bb7e20b0b2d3f
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2025-12-26 18:09:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ["8571782718:AAHIR4xEKr0NrPQU7eQIfZBpHQJ4PKsI_OU"]
 
 # Если пользователь не выбрал регион — считаем Москву
 DEFAULT_TZ = os.environ.get("DEFAULT_TZ", "Europe/Moscow")
@@ -8,3 +8,4 @@ DEFAULT_TZ = os.environ.get("DEFAULT_TZ", "Europe/Moscow")
 # Тихое правило
 INACTIVE_DAYS = int(os.environ.get("INACTIVE_DAYS", "7"))          # через сколько дней молчания писать
 NUDGE_COOLDOWN_DAYS = int(os.environ.get("NUDGE_COOLDOWN_DAYS", "7"))  # не чаще чем раз в N дней
+
